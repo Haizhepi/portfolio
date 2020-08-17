00c61dfb5369abaffadcb4c8336c9def676a42d9
Haizhepi <yunzheliu1998@gmail.com>
2020-08-16 20:56:21 -0700

    update

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar
+    elevation="16"
+    app
+    color="primary"
+  >
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+name: "NavBar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <NavBar/>
+    <v-main>
+
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from "@/components/NavBar";
+export default {
+  name: 'Home',
+  components: {
+    NavBar,
+  }
+}
+</script>
